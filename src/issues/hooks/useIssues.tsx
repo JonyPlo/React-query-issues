7492696c7fbff6d6947e1702693fc3ac1051a1ec
JonyPlo <joonyyplo@gmail.com>
2023-12-03 01:55:55 -0300

    feat: add pagination to the issues list

--- src/issues/hooks/useIssues.tsx
+++ src/issues/hooks/useIssues.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
+
 import { githubApi } from '../../api/githubApi'
 import { Issue, State } from '../interfaces'
 import { sleep } from '../helpers'
@@ -6,10 +8,15 @@ import { sleep } from '../helpers'
 interface Props {
   state?: State
   labels: string[]
+  page?: number
 }
 
 // Recordar que cuando se recibe un parametro que es opcional como el caso del "state?", este debe ponerse al final porque typescript valida los opcionales al final, es decir si los parametros se reciben de esta forma ", state?: State, label: string[]" esto daría error
-const getIssues = async (labels: string[], state?: State): Promise<Issue[]> => {
+const getIssues = async ({
+  labels,
+  state,
+  page = 1,
+}: Props): Promise<Issue[]> => {
   await sleep(2)
 
   const params = new URLSearchParams()
@@ -22,7 +29,7 @@ const getIssues = async (labels: string[], state?: State): Promise<Issue[]> => {
     params.append('labels', labelString)
   }
 
-  params.append('page', '1')
+  params.append('page', page.toString())
   params.append('per_page', '5')
 
   const { data } = await githubApi.get<Issue[]>('/issues', { params })
@@ -30,11 +37,40 @@ const getIssues = async (labels: string[], state?: State): Promise<Issue[]> => {
 }
 
 export const useIssues = ({ state, labels }: Props) => {
+  const [page, setPage] = useState(1)
+
+  useEffect(() => {
+    setPage(1)
+  }, [state, labels])
+
   const issuesQuery = useQuery({
     // Si dentro de la key, agregamos un objeto con elementos, no importa si antes tenia un orden y despues ese orden dentro del objeto cambio, react quey sabe detectar que quiero realizar cambios en esa key que ya esta almacenada en cache y no crear una key nueva
-    queryKey: ['issues', { state, labels }],
-    queryFn: () => getIssues(labels, state),
+    //Si una de las propiedades dentro del objeto en el queryKey cambia, hace que se vuelva a disparar la peticion
+    queryKey: ['issues', { state, labels, page }],
+    queryFn: () => getIssues({ labels, state, page }),
   })
 
-  return issuesQuery
+  const nextPage = () => {
+    if (issuesQuery.data?.length === 0) return
+
+    setPage(page + 1)
+
+    // issuesQuery.refetch() <-- Este metodo se usa cuando queremos re disparar la peticion por si hace falta
+  }
+
+  const prevPage = () => {
+    if (page > 1) setPage(page - 1)
+  }
+
+  return {
+    // Properties
+    issuesQuery,
+
+    // Getter
+    page: issuesQuery.isFetching ? 'Loading' : page,
+
+    // Methods
+    nextPage,
+    prevPage,
+  }
 }
--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -11,7 +11,10 @@ export const ListView = () => {
   const [selectedLabels, setSelectedLabels] = useState<string[]>([])
   const [state, setState] = useState<State>()
 
-  const issuesQuery = useIssues({state, labels: selectedLabels})
+  const { issuesQuery, page, nextPage, prevPage } = useIssues({
+    state,
+    labels: selectedLabels,
+  })
 
   const onChangeLabel = (labelName: string) => {
     selectedLabels.includes(labelName)
@@ -31,6 +34,23 @@ export const ListView = () => {
             onStateChange={(newState) => setState(newState)}
           />
         )}
+        <div className='d-flex mt-2 justify-content-between align-items-center'>
+          <button
+            className='btn btn-outline-primary'
+            onClick={prevPage}
+            disabled={issuesQuery.isFetching}
+          >
+            Prev
+          </button>
+          <span>{page}</span>
+          <button
+            className='btn btn-outline-primary'
+            onClick={nextPage}
+            disabled={issuesQuery.isFetching}
+          >
+            Next
+          </button>
+        </div>
       </div>
 
       <div className='col-4'>
